6c5e7f6ea8c6f906b32526cccf1969ceb87e5caf
patocanz <pato@canzeco.com>
2026-07-27 15:16:48 -0600

    Unexport file-private BoxTint (mobile-consumer)

--- apps/mobile-consumer/src/components/ui/BoxRow.tsx
+++ apps/mobile-consumer/src/components/ui/BoxRow.tsx
@@ -7,7 +7,7 @@ import { Pressable, Text, View } from 'react-native';
 import { GRADIENT_DIAGONAL, GRADIENTS } from '@/constants/brand';
 import { useReduceMotion } from '@/lib/useReduceMotion';
 
-export type BoxTint =
+type BoxTint =
   | 'pink'
   | 'sky'
   | 'emerald'
