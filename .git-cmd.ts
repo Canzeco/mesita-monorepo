346849983d8113b6c19f7b411a4363cfb7b7f3de
patocanz <pato@canzeco.com>
2026-09-23 18:33:40 -0600

    chore: remove git helper script

--- .git-cmd.ts
+++
@@ -1,9 +0,0 @@
-const cwd = Deno.cwd();
-const msg = Deno.args[0] ?? "fix";
-await new Deno.Command("git", { args: ["add", "-A"], cwd }).output();
-const out = await new Deno.Command("git", { args: ["commit", "-m", msg], cwd }).output();
-console.error(new TextDecoder().decode(out.stderr));
-if (!out.success) Deno.exit(out.code);
-const push = await new Deno.Command("git", { args: ["push"], cwd }).output();
-console.error(new TextDecoder().decode(push.stderr));
-Deno.exit(push.code);
